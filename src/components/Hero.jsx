2ab0da299f908e318dd446827c7048690c8d8ae6
bennyrak <brakower1@unc.edu>
2026-01-04 21:40:09 -0500

    added my last name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915eff]'>Benny</span>
+            Hi, I'm <span className='text-[#915eff]'>Benny Rakower</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a passionate full-stack web developer with a<br className='sm:block hidden'/> strong background using React and Angular frameworks.<br></br> Feel free to spin the desk below! 🔄<br></br>
